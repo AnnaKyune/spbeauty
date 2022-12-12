f7f7b1c5fb433eb6a9df561a7793fdabe07901fd
AnnaKyune <109480440+AnnaKyune@users.noreply.github.com>
2022-12-12 13:58:27 +0000

    Бордер с паддингом вокруг Addresses

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,16 +1,25 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
+import styled from "styled-components";
+
+const Address = styled.div`
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    padding: 24px;
+`;
+
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 m-4 col'>
+        <Address className='d-inline-flex flex-column gap-2 col'>
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
-        </div>
+        </Address>
     );
 }
 
