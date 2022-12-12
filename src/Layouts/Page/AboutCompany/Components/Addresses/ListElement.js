b01eb931b39274136c660511e58c583254296a16
AnnaKyune <109480440+AnnaKyune@users.noreply.github.com>
2022-12-12 15:18:43 +0000

    Выровнены паддинги ListElements левый отступ

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -15,7 +15,7 @@ const Option = styled.div`
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
+            <Option className='flex-grow-1 pe-2 pt-2 pb-2'>{props.children}</Option>
             <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
             <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
         </div>
